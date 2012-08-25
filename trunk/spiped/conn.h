44fd54c41c2e843f0619feb8e4676c20b707be17
cperciva <cperciva@592f418a-0512-ad62-f63d-38f288cbaac7>
2012-08-25 06:04:25 +0000

    Separate the "accepting connections" code from the "setting up and tearing down a single connection" code.  (These will be split into separate files soon.)

--- trunk/spiped/conn.h
+++ trunk/spiped/conn.h
@@ -3,10 +3,21 @@
 
 #include <stdint.h>
 
-#include "sock.h"
-
-/* Opaque structure. */
+/* Opaque structures. */
 struct proto_secret;
+struct sock_addr;
+
+/**
+ * conn_create(s, sas, decr, nofps, K, timeo, callback_dead, cookie):
+ * Create a connection with one end at ${s} and the other end connecting to
+ * the target addresses ${sas}.  If ${decr} is 0, encrypt the outgoing data;
+ * if ${decr} is nonzero, decrypt the outgoing data.  If ${nofps} is non-zero,
+ * don't use perfect forward secrecy.  Drop the connection if the handshake or
+ * connecting to the target takes more than ${timeo} seconds.  When the
+ * connection is dropped, invoke ${callback_dead}(${cookie}).
+ */
+int conn_create(int, struct sock_addr * const *, int, int,
+    const struct proto_secret *, double, int (*)(void *), void *);
 
 /**
  * conn_accept(s, sas, decr, nofps, K, nconn_max, timeo):
