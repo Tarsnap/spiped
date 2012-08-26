2f5bfb682b24e30eafaf7256b2a7115816f265fe
Colin Percival <cperciva@tarsnap.com>
2012-08-26 01:47:53 +0000

    Move code for managing a complete connection lifetime from spiped/conn.* to proto/proto_conn.*.

--- proto/proto_conn.h
+++ proto/proto_conn.h
@@ -1,12 +1,12 @@
-#ifndef _CONN_H_
-#define _CONN_H_
+#ifndef _PROTO_CONN_H_
+#define _PROTO_CONN_H_
 
 /* Opaque structures. */
 struct proto_secret;
 struct sock_addr;
 
 /**
- * conn_create(s, sas, decr, nofps, K, timeo, callback_dead, cookie):
+ * proto_conn_create(s, sas, decr, nofps, K, timeo, callback_dead, cookie):
  * Create a connection with one end at ${s} and the other end connecting to
  * the target addresses ${sas}.  If ${decr} is 0, encrypt the outgoing data;
  * if ${decr} is nonzero, decrypt the outgoing data.  If ${nofps} is non-zero,
@@ -14,7 +14,7 @@ struct sock_addr;
  * connecting to the target takes more than ${timeo} seconds.  When the
  * connection is dropped, invoke ${callback_dead}(${cookie}).
  */
-int conn_create(int, struct sock_addr * const *, int, int,
+int proto_conn_create(int, struct sock_addr * const *, int, int,
     const struct proto_secret *, double, int (*)(void *), void *);
 
 #endif /* !_CONN_H_ */
