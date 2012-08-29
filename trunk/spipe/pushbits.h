cbc46260d70a6da599ef3f1d3af36eb8ff2bc582
cperciva <cperciva@592f418a-0512-ad62-f63d-38f288cbaac7>
2012-08-29 11:31:42 +0000

    Add new "spipe" utility.  This is a client which creates a single spiped connection, sending data read from the standard input and writing data it receives back out to the standard output.
    The principle intended use case for this is as an SSH ProxyCommand to make
    it easier to hide SSH servers behind spiped.

---
+++ trunk/spipe/pushbits.h
@@ -0,0 +1,10 @@
+#ifndef _PUSHBITS_H_
+#define _PUSHBITS_H_
+
+/**
+ * pushbits(in, out):
+ * Create a thread which copies data from ${in} to ${out}.
+ */
+int pushbits(int, int);
+
+#endif /* !_PUSHBITS_H_ */
