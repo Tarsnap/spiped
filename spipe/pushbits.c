1874ee73cb90aed1e70a1d2d06b500ac56c19b95
Colin Percival <cperciva@tarsnap.com>
2012-08-29 11:31:42 +0000

    Add new "spipe" utility.  This is a client which creates a single spiped connection, sending data read from the standard input and writing data it receives back out to the standard output.
    The principle intended use case for this is as an SSH ProxyCommand to make
    it easier to hide SSH servers behind spiped.

---
+++ spipe/pushbits.c
@@ -0,0 +1,113 @@
+#include <sys/socket.h>
+
+#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "warnp.h"
+
+#include "pushbits.h"
+
+struct push {
+	uint8_t buf[BUFSIZ];
+	int in;
+	int out;
+};
+
+/* Bit-pushing thread. */
+static void *
+workthread(void * cookie)
+{
+	struct push * P = cookie;
+	ssize_t readlen;
+	ssize_t pos;
+	ssize_t writlen;
+
+	/* Infinite loop unless we hit EOF or an error. */
+	do {
+		/* Read data and die on error. */
+		if ((readlen = read(P->in, P->buf, BUFSIZ)) == -1) {
+			if (errno == EINTR)
+				continue;
+			warnp("Error reading");
+			exit(1);
+		}
+
+		/* If we hit EOF, exit the loop. */
+		if (readlen == 0)
+			break;
+
+		/* Write the data back out. */
+		for (pos = 0; pos < readlen; pos += writlen) {
+			/* Write data and die on error. */
+			if ((writlen = write(P->out, &P->buf[pos],
+			    readlen - pos)) == -1) {
+				if (errno == EINTR) {
+					writlen = 0;
+					continue;
+				}
+				warnp("Error writing");
+				exit(1);
+			}
+
+			/* We should have written at least one byte. */
+			assert(writlen >= 1);
+		}
+	} while (1);
+
+	/* Close the descriptor we hit EOF on. */
+	close(P->in);
+
+	/*
+	 * Try to shut down the descriptor we're writing to.  Ignore ENOTSOCK,
+	 * since it might, indeed, not be a socket.
+	 */
+	if (shutdown(P->out, SHUT_WR)) {
+		if (errno != ENOTSOCK) {
+			warnp("Error shutting down socket");
+			exit(1);
+		}
+	}
+
+	/* Free our parameters. */
+	free(P);
+
+	/* We're done. */
+	return (NULL);
+}
+
+/**
+ * pushbits(in, out):
+ * Create a thread which copies data from ${in} to ${out}.
+ */
+int
+pushbits(int in, int out)
+{
+	struct push * P;
+	pthread_t thr;
+	int rc;
+
+	/* Allocate structure. */
+	if ((P = malloc(sizeof(struct push))) == NULL)
+		goto err0;
+	P->in = in;
+	P->out = out;
+
+	/* Create thread. */
+	if ((rc = pthread_create(&thr, NULL, workthread, P)) != 0) {
+		warn0("pthread_create: %s", strerror(rc));
+		goto err1;
+	}
+
+	/* Success! */
+	return (0);
+
+err1:
+	free(P);
+err0:
+	/* Failure! */
+	return (-1);
+}
