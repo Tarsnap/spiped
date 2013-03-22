3c835d1949c88aa4cd9e1a58c4d87aab1b3608b7
cperciva <cperciva@592f418a-0512-ad62-f63d-38f288cbaac7>
2013-03-22 00:22:51 +0000

    Enable protocol-layer keep-alives by default.  (Some operating systems do this already.)  Add a new -j option to turn keep-alives off.

--- trunk/proto/proto_conn.c
+++ trunk/proto/proto_conn.c
@@ -1,3 +1,5 @@
+#include <sys/socket.h>
+
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -16,6 +18,7 @@ struct conn_state {
 	void * cookie;
 	int decr;
 	int nofps;
+	int nokeepalive;
 	const struct proto_secret * K;
 	double timeo;
 	int s;
@@ -69,6 +72,17 @@ err0:
 static int
 launchpipes(struct conn_state * C)
 {
+	int on = C->nokeepalive ? 0 : 1;
+
+	/*
+	 * Attempt to turn keepalives on or off as requested.  We ignore
+	 * failures here since the sockets might not be of a type for which
+	 * SO_KEEPALIVE is valid -- it is a socket level option, but protocol
+	 * specific.  In particular, it has no sensible meaning for UNIX
+	 * sockets.
+	 */
+	setsockopt(C->s, SOL_SOCKET, SO_KEEPALIVE, &on, sizeof(on));
+	setsockopt(C->t, SOL_SOCKET, SO_KEEPALIVE, &on, sizeof(on));
 
 	/* Create two pipes. */
 	if ((C->pipe_f = proto_pipe(C->s, C->t, C->decr, C->k_f,
@@ -134,17 +148,20 @@ dropconn(struct conn_state * C)
 }
 
 /**
- * proto_conn_create(s, sas, decr, nofps, K, timeo, callback_dead, cookie):
+ * proto_conn_create(s, sas, decr, nofps, nokeepalive, K, timeo,
+ *     callback_dead, cookie):
  * Create a connection with one end at ${s} and the other end connecting to
  * the target addresses ${sas}.  If ${decr} is 0, encrypt the outgoing data;
  * if ${decr} is nonzero, decrypt the outgoing data.  If ${nofps} is non-zero,
- * don't use perfect forward secrecy.  Drop the connection if the handshake or
- * connecting to the target takes more than ${timeo} seconds.  When the
- * connection is dropped, invoke ${callback_dead}(${cookie}).
+ * don't use perfect forward secrecy.  Enable transport layer keep-alives (if
+ * applicable) on both sockets if and only if ${nokeepalive} is zero.  Drop the
+ * connection if the handshake or connecting to the target takes more than
+ * ${timeo} seconds.  When the connection is dropped, invoke
+ * ${callback_dead}(${cookie}).
  */
 int
 proto_conn_create(int s, struct sock_addr * const * sas, int decr, int nofps,
-    const struct proto_secret * K, double timeo,
+    int nokeepalive, const struct proto_secret * K, double timeo,
     int (* callback_dead)(void *), void * cookie)
 {
 	struct conn_state * C;
@@ -156,6 +173,7 @@ proto_conn_create(int s, struct sock_addr * const * sas, int decr, int nofps,
 	C->cookie = cookie;
 	C->decr = decr;
 	C->nofps = nofps;
+	C->nokeepalive = nokeepalive;
 	C->K = K;
 	C->timeo = timeo;
 	C->s = s;
--- trunk/spiped/dispatch.h
+++ trunk/spiped/dispatch.h
@@ -8,15 +8,16 @@ struct proto_secret;
 struct sock_addr;
 
 /**
- * dispatch_accept(s, sas, decr, nofps, K, nconn_max, timeo):
+ * dispatch_accept(s, sas, decr, nofps, nokeepalive, K, nconn_max, timeo):
  * Start accepting connections on the socket ${s}.  Connect to the target
  * addresses ${sas}.  If ${decr} is 0, encrypt the outgoing connections; if
  * ${decr} is non-zero, decrypt the incoming connections.  Don't accept more
  * than ${nconn_max} connections.  If ${nofps} is non-zero, don't use perfect
- * forward secrecy.  Drop connections if the handshake or connecting to the
- * target takes more than ${timeo} seconds.
+ * forward secrecy.  Enable transport layer keep-alives (if applicable) if and
+ * only if ${nokeepalive} is zero.  Drop connections if the handshake or
+ * connecting to the target takes more than ${timeo} seconds.
  */
-int dispatch_accept(int, struct sock_addr * const *, int, int,
+int dispatch_accept(int, struct sock_addr * const *, int, int, int,
     const struct proto_secret *, size_t, double);
 
 #endif /* !_DISPATCH_H_ */
