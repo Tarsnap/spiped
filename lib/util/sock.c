d2a11c7aedf1de4d1c4a38d31c9dc7460bd0904f
Colin Percival <cperciva@tarsnap.com>
2013-03-28 10:10:59 +0000

    MFlibcperciva r23:   Add new functions: sock_addr_{cmp, dup, serialize, deserialize}.
      Move definition of struct sock_addr into a new sock_internal.h.
    
      Add code for prettyprinting socket addresses in a manner suitable for
      writing to log files.

--- lib/util/sock.c
+++ lib/util/sock.c
@@ -17,14 +17,7 @@
 #include "warnp.h"
 
 #include "sock.h"
-
-/* Socket address structure. */
-struct sock_addr {
-	int ai_family;
-	int ai_socktype;
-	struct sockaddr * name;
-	socklen_t namelen;
-};
+#include "sock_internal.h"
 
 /* Convert a path into a socket address. */
 static struct sock_addr **
