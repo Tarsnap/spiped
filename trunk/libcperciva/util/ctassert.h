e4b8d0bcd3abade913fea217ce6fe80ebc166889
cperciva <cperciva@592f418a-0512-ad62-f63d-38f288cbaac7>
2013-08-19 07:17:29 +0000

    MFlibcperciva r39:   Avoid namespace collisions -- some platforms provide CTASSERT.

--- trunk/libcperciva/util/ctassert.h
+++ trunk/libcperciva/util/ctassert.h
@@ -5,10 +5,15 @@
  * CTASSERT(foo) will produce a compile-time error if "foo" is not a constant
  * expression which evaluates to a non-zero value.
  */
-#ifndef CTASSERT
-#define CTASSERT(x)             _CTASSERT(x, __LINE__)
-#define _CTASSERT(x, y)         __CTASSERT(x, y)
-#define __CTASSERT(x, y)        typedef char __assert ## y[(x) ? 1 : -1]
+
+/* Kill any existing definition, just in case it's different. */
+#ifdef CTASSERT
+#undef CTASSERT
 #endif
+
+/* Define using libcperciva namespace to avoid collisions. */
+#define CTASSERT(x)			libcperciva_CTASSERT(x, __LINE__)
+#define libcperciva_CTASSERT(x, y)	libcperciva__CTASSERT(x, y)
+#define libcperciva__CTASSERT(x, y)	typedef char libcperciva__assert ## y[(x) ? 1 : -1]
 
 #endif /* !_CTASSERT_H_ */
