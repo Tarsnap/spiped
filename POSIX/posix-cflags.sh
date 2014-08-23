db2025feb207570979e158cfbfb676e79c60f7c1
Colin Percival <cperciva@tarsnap.com>
2014-08-23 05:09:24 +0000

    Rename posix-msg_nosignal.sh to posix-cflags.sh so that we can add other flags to it in the future.

--- POSIX/posix-cflags.sh
+++ POSIX/posix-cflags.sh
@@ -1,4 +1,4 @@
-# Should be sourced by `command -p sh posix-msg_nosignal.sh` from within a Makefile
+# Should be sourced by `command -p sh posix-cflags.sh` from within a Makefile
 if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-msg_nosignal.c 2>/dev/null; then
 	echo "-DPOSIXFAIL_MSG_NOSIGNAL"
 	echo "WARNING: POSIX violation: <sys/socket.h> not defining MSG_NOSIGNAL" >/dev/stderr
