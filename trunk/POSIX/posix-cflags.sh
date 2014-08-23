0243f11d41fa5590ea990260d586dbae9ade6306
cperciva <cperciva@592f418a-0512-ad62-f63d-38f288cbaac7>
2014-08-23 07:47:10 +0000

    Change monoclock to use clock_gettime(CLOCK_REALTIME) as a fallback when CLOCK_MONOTONIC does not succeed.  Since CLOCK_REALTIME is required by POSIX to be implemented, this avoids the need for gettimeofday, which is a non-POSIX function.
    Since clock_gettime is a fairly recent addition to POSIX, retain the
    option for gettimeofday if CLOCK_REALTIME is not defined; but wrap it in
    POSIXFAIL_CLOCK_REALTIME.
    
    Add code for detecting systems with this POSIX-compliance bug and defining
    the macro.

--- trunk/POSIX/posix-cflags.sh
+++ trunk/POSIX/posix-cflags.sh
@@ -3,4 +3,8 @@ if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-msg_nosignal.c 2>/dev/null; then
 	echo "-DPOSIXFAIL_MSG_NOSIGNAL"
 	echo "WARNING: POSIX violation: <sys/socket.h> not defining MSG_NOSIGNAL" >/dev/stderr
 fi
+if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-clock_realtime.c 2>/dev/null; then
+	echo "-DPOSIXFAIL_CLOCK_REALTIME"
+	echo "WARNING: POSIX violation: <time.h> not defining CLOCK_REALTIME" >/dev/stderr
+fi
 rm -f a.out
