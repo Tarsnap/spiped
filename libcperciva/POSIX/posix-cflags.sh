5d68e5751554395c5ae01ea8462212ec85be26e2
Colin Percival <cperciva@tarsnap.com>
2016-06-26 19:35:36 -0700

    Adjust POSIX-compatibility cflag gathering
    As in posix-l.sh, keep track of whether we are printing the first
    flag and add a space before the upcoming flag if not.

--- libcperciva/POSIX/posix-cflags.sh
+++ libcperciva/POSIX/posix-cflags.sh
@@ -1,9 +1,12 @@
 # Should be sourced by `command -p sh posix-cflags.sh` from within a Makefile
+FIRST=YES
 if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-msg_nosignal.c 2>/dev/null; then
-	printf %s "-DPOSIXFAIL_MSG_NOSIGNAL "
+	[ ${FIRST} == "NO" ] && printf " "; FIRST=NO
+	printf %s "-DPOSIXFAIL_MSG_NOSIGNAL"
 	echo "WARNING: POSIX violation: <sys/socket.h> not defining MSG_NOSIGNAL" 1>&2
 fi
 if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-clock_realtime.c 2>/dev/null; then
+	[ ${FIRST} == "NO" ] && printf " "; FIRST=NO
 	printf %s "-DPOSIXFAIL_CLOCK_REALTIME"
 	echo "WARNING: POSIX violation: <time.h> not defining CLOCK_REALTIME" 1>&2
 fi
