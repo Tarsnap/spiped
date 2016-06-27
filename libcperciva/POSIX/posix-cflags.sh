2639b0c78f649469b2d7a6d4c9b24f677554b7f1
Colin Percival <cperciva@tarsnap.com>
2016-06-27 00:01:06 -0700

    Typo fix: [ uses '=', not '=='.

--- libcperciva/POSIX/posix-cflags.sh
+++ libcperciva/POSIX/posix-cflags.sh
@@ -1,19 +1,19 @@
 # Should be sourced by `command -p sh posix-cflags.sh` from within a Makefile
 FIRST=YES
 if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-msg_nosignal.c 2>/dev/null; then
-	[ ${FIRST} == "NO" ] && printf " "; FIRST=NO
+	[ ${FIRST} = "NO" ] && printf " "; FIRST=NO
 	printf %s "-DPOSIXFAIL_MSG_NOSIGNAL"
 	echo "WARNING: POSIX violation: <sys/socket.h> not defining MSG_NOSIGNAL" 1>&2
 fi
 if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-clock_realtime.c 2>/dev/null; then
-	[ ${FIRST} == "NO" ] && printf " "; FIRST=NO
+	[ ${FIRST} = "NO" ] && printf " "; FIRST=NO
 	printf %s "-DPOSIXFAIL_CLOCK_REALTIME"
 	echo "WARNING: POSIX violation: <time.h> not defining CLOCK_REALTIME" 1>&2
 fi
 if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-restrict.c 2>/dev/null; then
 	echo "WARNING: POSIX violation: ${CC} does not accept 'restrict' keyword" 1>&2
 	if ${CC} -D_POSIX_C_SOURCE=200809L -std=c99 posix-restrict.c 2>/dev/null; then
-		[ ${FIRST} == "NO" ] && printf " "; FIRST=NO
+		[ ${FIRST} = "NO" ] && printf " "; FIRST=NO
 		printf %s "-std=c99"
 	fi
 fi
