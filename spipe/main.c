e22da89b4f9e78828c9e0970d1e4ffedd7cb94cb
Graham Percival <gperciva@tarsnap.com>
2016-06-29 12:38:35 -0700

    spipe: begin adding error path and cleanup

--- spipe/main.c
+++ spipe/main.c
@@ -136,17 +136,17 @@ main(int argc, char * argv[])
 	/* Resolve target address. */
 	if ((sas_t = sock_resolve(opt_t)) == NULL) {
 		warnp("Error resolving socket address: %s", opt_t);
-		exit(1);
+		goto err0;
 	}
 	if (sas_t[0] == NULL) {
 		warn0("No addresses found for %s", opt_t);
-		exit(1);
+		goto err1;
 	}
 
 	/* Load the keying data. */
 	if ((K = proto_crypt_secret(opt_k)) == NULL) {
 		warnp("Error reading shared secret");
-		exit(1);
+		goto err1;
 	}
 
 	/*
@@ -159,14 +159,14 @@ main(int argc, char * argv[])
 	 */
 	if (socketpair(AF_UNIX, SOCK_STREAM, 0, s)) {
 		warnp("socketpair");
-		exit(1);
+		goto err2;
 	}
 
 	/* Set up a connection. */
 	if (proto_conn_create(s[1], sas_t, 0, opt_f, opt_g, opt_j, K, opt_o,
 	    callback_conndied, NULL) == NULL) {
 		warnp("Could not set up connection");
-		exit(1);
+		goto err2;
 	}
 
 	/* Push bits from stdin into the socket. */
@@ -184,8 +184,12 @@ main(int argc, char * argv[])
 	} while (1);
 
 	/* NOTREACHED */
-	/*
-	 * If we could reach this point, we would free memory, close sockets,
-	 * and otherwise clean up here.
-	 */
+
+err2:
+	free(K);
+err1:
+	sock_addr_freelist(sas_t);
+err0:
+	/* Failure! */
+	exit(1);
 }
