3aa07a1d245b50aa82106c7b68788d7994acba3e
Graham Percival <gperciva@tarsnap.com>
2017-10-04 15:39:41 -0700

    POSIX: add POSIXFAIL_CLOCK_GETTIME for clock_gettime()
    OS X 10.11 and below did not include clock_gettime(), despite it being a
    mandatory part of POSIX.
    
    To make things worse, on OS X 10.11 with XCode 8, clock_gettime() and
    CLOCK_REALTIME are defined in <time.h> despite the symbol not being exported by
    the system libraries; so we have to detect this case by trying to run code
    which uses clock_gettime rather than relying on whether preprocessor macros are
    defined.
    
    These problems do not affect OS X 10.12.

--- libcperciva/POSIX/posix-cflags.sh
+++ libcperciva/POSIX/posix-cflags.sh
@@ -21,6 +21,25 @@ if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-clock_realtime.c 2>/dev/null; then
 	printf %s "-DPOSIXFAIL_CLOCK_REALTIME"
 	echo "WARNING: POSIX violation: <time.h> not defining CLOCK_REALTIME" 1>&2
 fi
+if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-clock_gettime.c 2>/dev/null; then
+	[ ${FIRST} = "NO" ] && printf " "; FIRST=NO
+	printf %s "-DPOSIXFAIL_CLOCK_GETTIME"
+	echo "WARNING: POSIX violation: <time.h> not declaring clock_gettime()" 1>&2
+else
+	# Even if the compilation succeeds, we still need to run the binary
+	# because OS X 10.11 with XCode 8 _will_ contain clock_gettime() in the
+	# header (because it was added in 10.12 and they only use one SDK per
+	# XCode version), but it will link to the 10.11 library (which doesn't
+	# include it).  Annoyingly, there's two levels of error output on OS X:
+	# one from the binary itself, and one from the signal it sends to the
+	# calling process.  The "$( ./x 2>y ) 2>y" captures both types of error
+	# message.
+	if ! $( ./a.out 2>/dev/null ) 2>/dev/null ; then
+		[ ${FIRST} = "NO" ] && printf " "; FIRST=NO
+		printf %s "-DPOSIXFAIL_CLOCK_GETTIME"
+		echo "WARNING: POSIX violation: clock_gettime() is not linkable" 1>&2
+	fi
+fi
 if ! ${CC} -D_POSIX_C_SOURCE=200809L posix-restrict.c 2>/dev/null; then
 	echo "WARNING: POSIX violation: ${CC} does not accept 'restrict' keyword" 1>&2
 	if ${CC} -D_POSIX_C_SOURCE=200809L -std=c99 posix-restrict.c 2>/dev/null; then
