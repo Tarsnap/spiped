f4d4d909f9ca9da6b8f47928afb1d5fdacc8f04e
Graham Percival <gperciva@tarsnap.com>
2018-07-10 10:54:22 -0700

    tests/valgrind: potential memleak in pthread_join

--- tests/valgrind/potential-memleaks.c
+++ tests/valgrind/potential-memleaks.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,12 +10,32 @@ pl_freebsd_link_lrt(void)
 	/* Do nothing. */
 }
 
+/* Part of the pthread init. */
+static void *
+do_nothing(void * cookie)
+{
+
+	(void)cookie;
+	return (NULL);
+}
+
+/* Problem with FreeBSD 11.0 and creating/freeing a thread. */
+static void
+pl_freebsd_pthread(void)
+{
+	pthread_t thread;
+
+	pthread_create(&thread, NULL, do_nothing, NULL);
+	pthread_join(thread, NULL);
+}
+
 #define MEMLEAKTEST(x) { #x, x }
 static const struct memleaktest {
 	const char * name;
 	void (* func)(void);
 } tests[] = {
-	MEMLEAKTEST(pl_freebsd_link_lrt)
+	MEMLEAKTEST(pl_freebsd_link_lrt),
+	MEMLEAKTEST(pl_freebsd_pthread)
 };
 static const int num_tests = sizeof(tests) / sizeof(tests[0]);
 
