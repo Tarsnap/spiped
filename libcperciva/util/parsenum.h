401840886e2f474dac5af12d207070ec2ea5a97c
Graham Percival <gperciva@tarsnap.com>
2019-08-14 09:51:33 -0700

    parsenum: add PARSENUM_BASE
    This borrows heavily from PARSENUM, with the following changes:
    - takes an argument b (base), instead of using the hard-coded base of 0.
    - expands PARSENUM_BASE to PARSENUM_BASE3 or PARSENUM_BASE5.
    - gives an ASSERT_FAIL if x is a float

--- libcperciva/util/parsenum.h
+++ libcperciva/util/parsenum.h
@@ -94,6 +94,59 @@ _Pragma("clang diagnostic pop")
 #define PARSENUM_COUNT(...)	PARSENUM_COUNT_(__VA_ARGS__, 4, 3, 2, 1)
 #define PARSENUM_COUNT_(_1, _2, _3, _4, N, ...)	N
 
+/**
+ * PARSENUM_BASE(x, s, min, max, base):
+ * Parse the string ${s} according to the type of the unsigned integer or
+ * signed integer variable ${x}, in the specified ${base}.  If the string
+ * consists of optional whitespace followed by a number (and nothing else) and
+ * the numeric interpretation of the number is between ${min} and ${max}
+ * inclusive, store the value into ${x}, set errno to zero, and return zero.
+ * Otherwise, return nonzero with an unspecified value of ${x} and errno set
+ * to EINVAL or ERANGE as appropriate.
+ *
+ * For an unsigned integer variable ${x}, this can also be invoked as
+ * PARSENUM_BASE(x, s, base), in which case the minimum and maximum values are
+ * set to the limits of the unsigned integer type.
+ */
+#define PARSENUM_BASE3(x, s, b)						\
+	(								\
+		PARSENUM_PROLOGUE					\
+		errno = 0,						\
+		(((*(x)) = 1, (*(x)) /= 2) > 0)	?			\
+			(ASSERT_FAIL("PARSENUM_BASE applied to float"), 1) : \
+		(((*(x)) = -1) > 0) ?					\
+			((*(x)) = parsenum_unsigned((s), 0, (*(x)),	\
+			    (*(x)), (b))) :				\
+			(ASSERT_FAIL("PARSENUM_BASE applied to signed integer without specified bounds"), 1),	\
+		errno != 0						\
+		PARSENUM_EPILOGUE					\
+	)
+#define PARSENUM_BASE5(x, s, min, max, base)				\
+	(								\
+		PARSENUM_PROLOGUE					\
+		errno = 0,						\
+		(((*(x)) = 1, (*(x)) /= 2) > 0)	?			\
+			(ASSERT_FAIL("PARSENUM_BASE applied to float"), 1) : \
+		(((*(x)) = -1) <= 0) ?					\
+			((*(x)) = parsenum_signed((s),			\
+			    (*(x) <= 0) ? (min) : 0,			\
+			    (*(x) <= 0) ? (max) : 0, base)) :		\
+			(((*(x)) = parsenum_unsigned((s),		\
+			    (min) <= 0 ? 0 : (min),			\
+				(uintmax_t)(max), *(x), base)),		\
+			((((max) < 0) && (errno == 0)) ?		\
+			    (errno = ERANGE) : 0)),			\
+		errno != 0						\
+		PARSENUM_EPILOGUE					\
+	)
+
+/* Magic to select which version of PARSENUM_BASE to use. */
+#define PARSENUM_BASE(...)	PARSENUM_BASE_(PARSENUM_BASE_COUNT(__VA_ARGS__))(__VA_ARGS__)
+#define PARSENUM_BASE_(N)	PARSENUM_BASE__(N)
+#define PARSENUM_BASE__(N)	PARSENUM_BASE ## N
+#define PARSENUM_BASE_COUNT(...)	PARSENUM_BASE_COUNT_(__VA_ARGS__, 5, 4, 3, 2, 1)
+#define PARSENUM_BASE_COUNT_(_1, _2, _3, _4, _5, N, ...)	N
+
 /* Functions for performing the parsing and parameter checking. */
 static inline double
 parsenum_float(const char * s, double min, double max)
