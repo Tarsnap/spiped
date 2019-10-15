a9c2a435258668e62e8ea2ee722ac789cf71c51d
Graham Percival <graham@percival-music.ca>
2019-10-15 15:39:07 -0700

    tests: add spipe with bps-limited echo server
    When cat finishes working its way through the file and spipe reads the
    EOF, it will close the _sending_ side of the TCP connection to the
    remote spiped process, but the _receiving_ side needs to keep on working
    so that we can read the data on its way back.
    
    tests/lorem-send.txt is 446 characters, and we're limiting the echo
    server to 1000 bytes per second.  I think that ~500 ms is a good target
    for this test, but we can tweak these values if necessary.

---
+++ tests/08-send-data-spipe-limited-bps.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Goal of this test:
+# - ensure that spipe can close the sending side of the TCP
+#   connection, while the receiving side keeps working.
+
+### Constants
+echo_limited_output="${s_basename}-echo-limited-output.txt"
+
+### Actual command
+scenario_cmd() {
+	# Send a file through a rate-limited echo server.  Echo
+	# server is limited to 1000 bytes per second.
+	setup_check_variables
+	setup_spiped_decryption_server /dev/null 0 1 1000
+	cat ${scriptdir}/lorem-send.txt				\
+		| ${spipe_binary} -t ${mid_sock} -k /dev/null	\
+		> ${echo_limited_output}
+	echo $? > ${c_exitfile}
+
+	setup_check_variables
+	cmp -s ${scriptdir}/lorem-send.txt ${echo_limited_output}
+	echo $? > ${c_exitfile}
+
+	# Clean up
+	servers_stop
+}
