9079f406eb7ad5bf903d7863ddf8a4023cb37c0e
Graham Percival <gperciva@tarsnap.com>
2021-07-13 12:39:39 -0700

    perftest: allow ${init_func} to be NULL, add ${clean_func}

--- libcperciva/util/perftest.h
+++ libcperciva/util/perftest.h
@@ -6,17 +6,21 @@
 
 /**
  * perftest_buffers(nbytes, sizes, nsizes, nbytes_warmup, init_func, func,
- *     cookie):
+ *     clean_func, cookie):
  * Time using ${func} to process ${nbytes} bytes in blocks of ${sizes}.
  * Before timing any block sizes, process ${nbytes_warmup} bytes with the
  * maximum size in ${sizes}.  Invoke callback functions as:
  *     init_func(cookie, buffer, buflen)
  *     func(cookie, buffer, buflen, nbuffers)
+ *     clean_func(cookie)
  * where ${buffer} is large enough to hold the maximum buffer size.  Print
- * the time and speed of processing each buffer size.
+ * the time and speed of processing each buffer size.  ${init_func} and
+ * ${clean_func} may be NULL.  If ${init_func} has completed successfully,
+ * then ${clean_func} will be called if there is a subsequent error.
  */
 int perftest_buffers(size_t, const size_t *, size_t, size_t,
     int (*)(void *, uint8_t *, size_t),
-    int (*)(void *, uint8_t *, size_t, size_t), void *);
+    int (*)(void *, uint8_t *, size_t, size_t),
+    int (*)(void *), void *);
 
 #endif /* !_PERFTESTS_H_ */
