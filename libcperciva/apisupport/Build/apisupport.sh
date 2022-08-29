Tarsnap/libcperciva

apisupport: add NONPOSIX SETGROUPS check
setgroups() is not part of POSIX.  We've hardcoded -D_POSIX_C_SOURCE=200809L
-D_XOPEN_SOURCE=700 into our command-line arguments, and we're trying to
undo that in util/setgroups_none.c by having:
    /* We use non-POSIX functionality in this file. */
    #undef _POSIX_C_SOURCE
    #undef _XOPEN_SOURCE

However, that's not technically allowed under C99:

c99 7.1.3 Reserved identifiers:
    All identifiers that begin with an underscore and either an uppercase
    letter or another underscore are always reserved for any use.

    ...

    If the program removes (with #undef) any macro definition of an
    identifier in the first group listed above, the behavior is undefined.

The POSIX page for c99 states:
    -D  name[=value]
	Define name as if by a C-language #define directive. If no = value
	is given, a value of 1 shall be used. The -D option has lower
	precedence than the -U option. That is, if name is used in both a -U
	and a -D option, name shall be undefined regardless of the order of
	the options.
    ...
    -U  name
        Remove any initial definition of name.

In other words, `-U foo` is not the same thing as prepending `#undef foo`
to the top of the source file [1][2].

So our goal of this commit is to detect whether appending
-U_POSIX_C_SOURCE -U_XOPEN_SOURCE to the command-line will allow the
compiler to find setgroups().

[1] gcc and clang have slightly different descriptions of -D and -U: gcc
says that -U cancels any *previous* definition of foo, while clang says
that it adds an implicit #undef that is read before the source file is
processed.  But those compiler's lack of POSIX compliance is not our
responsibility... and in any case, I'm fairly confident that is a
completely theoretical issue.

[2] FWIW, FreeBSD's `man c99` matches the POSIX behaviour for -U,
whereas `man cc` has the non-POSIX clang behaviour for -U.

apisupport/Build/apisupport.sh
@@ -60,3 +60,8 @@ if [ "$2" = "--all" ]; then
 		echo "#define APISUPPORT_${PLATFORM}_${FEATURE} 1"
 	}
 fi
+
+# Detect how to compile non-POSIX code.
+feature NONPOSIX SETGROUPS "" ""			\
+	"-U_POSIX_C_SOURCE -U_XOPEN_SOURCE"		\
+	"-U_POSIX_C_SOURCE -U_XOPEN_SOURCE -Wno-reserved-macro-identifier"

Tarsnap/spiped

libcperciva/apisupport/Build/apisupport.sh
@@ -60,3 +60,8 @@ if [ "$2" = "--all" ]; then
 		echo "#define APISUPPORT_${PLATFORM}_${FEATURE} 1"
 	}
 fi
+
+# Detect how to compile non-POSIX code.
+feature NONPOSIX SETGROUPS "" ""			\
+	"-U_POSIX_C_SOURCE -U_XOPEN_SOURCE"		\
+	"-U_POSIX_C_SOURCE -U_XOPEN_SOURCE -Wno-reserved-id-macro"