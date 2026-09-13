7d973ce67d6c5d759c3d3d7ed81a3998a2acfc67
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-12 22:09:01 -0400

    test: exercise decrypt EOF frame boundaries

--- perftests/standalone-enc/standalone_pipe_socketpair_one.c
+++ perftests/standalone-enc/standalone_pipe_socketpair_one.c
@@ -31,6 +31,11 @@ struct pipeinfo {
 	int done;
 };
 
+struct eof_testinfo {
+	int status;
+	int done;
+};
+
 static int
 pipe_callback_status(void * cookie)
 {
@@ -44,6 +49,18 @@ pipe_callback_status(void * cookie)
 
 	/* We've finished. */
 	pipeinfo->done = 1;
+
+	/* Success! */
+	return (0);
+}
+
+static int
+pipe_callback_eof_test(void * cookie)
+{
+	struct eof_testinfo * info = cookie;
+
+	/* We've reached EOF or an error. */
+	info->done = 1;
 
 	/* Success! */
 	return (0);
@@ -191,6 +208,110 @@ err0:
 	return (-1);
 }
 
+static int
+pipe_decr_eof_case(size_t complete_frames, size_t tail_len,
+    int expected_status)
+{
+	struct eof_testinfo info;
+	struct proto_keys * enc = NULL;
+	struct proto_keys * dec = NULL;
+	void * pipe = NULL;
+	uint8_t kbuf[64];
+	uint8_t inbuf[PCRYPT_MAXDSZ];
+	uint8_t encbuf[PCRYPT_ESZ];
+	int in[2] = {-1, -1};
+	int out[2] = {-1, -1};
+	size_t i;
+
+	/* Set up matching encryption and decryption keys. */
+	memset(kbuf, 0, sizeof(kbuf));
+	memset(inbuf, 0, sizeof(inbuf));
+	if ((enc = mkkeypair(kbuf)) == NULL)
+		goto err0;
+	if ((dec = mkkeypair(kbuf)) == NULL)
+		goto err0;
+
+	/* Create socket pairs for the encrypted input and cleartext output. */
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, in)) {
+		warnp("socketpair");
+		goto err0;
+	}
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, out)) {
+		warnp("socketpair");
+		goto err0;
+	}
+
+	/* Start a decrypting pipe. */
+	info.status = 1;
+	info.done = 0;
+	if ((pipe = proto_pipe(in[R], out[W], 1, dec, &info.status,
+	    pipe_callback_eof_test, &info)) == NULL) {
+		warn0("proto_pipe");
+		goto err0;
+	}
+
+	/* Send any complete frames first. */
+	for (i = 0; i < complete_frames; i++) {
+		proto_crypt_enc(inbuf, sizeof(inbuf), encbuf, enc);
+		if (noeintr_write(in[W], encbuf, sizeof(encbuf))
+		    != (ssize_t)sizeof(encbuf)) {
+			warnp("write");
+			goto err0;
+		}
+	}
+
+	/* Send a truncated final frame if requested. */
+	if (tail_len != 0) {
+		proto_crypt_enc(inbuf, sizeof(inbuf), encbuf, enc);
+		if (noeintr_write(in[W], encbuf, tail_len) != (ssize_t)tail_len) {
+			warnp("write");
+			goto err0;
+		}
+	}
+
+	/* End the encrypted input and let the pipe report its final status. */
+	if (shutdown(in[W], SHUT_WR)) {
+		warnp("shutdown");
+		goto err0;
+	}
+	if (events_spin(&info.done)) {
+		warnp("events_spin");
+		goto err0;
+	}
+	if (info.status != expected_status) {
+		warn0("proto_pipe EOF status: got %d, expected %d",
+		    info.status, expected_status);
+		goto err0;
+	}
+
+	/* Clean up. */
+	proto_pipe_cancel(pipe);
+	proto_crypt_free(enc);
+	proto_crypt_free(dec);
+	(void)close(in[R]);
+	(void)close(in[W]);
+	(void)close(out[R]);
+	(void)close(out[W]);
+
+	/* Success! */
+	return (0);
+
+err0:
+	if (pipe != NULL)
+		proto_pipe_cancel(pipe);
+	proto_crypt_free(enc);
+	proto_crypt_free(dec);
+	if (in[R] != -1)
+		(void)close(in[R]);
+	if (in[W] != -1)
+		(void)close(in[W]);
+	if (out[R] != -1)
+		(void)close(out[R]);
+	if (out[W] != -1)
+		(void)close(out[W]);
+	return (-1);
+}
+
 /**
  * standalone_pipe_socketpair_one(perfsizes, num_perf, nbytes_perftest,
  *     nbytes_warmup):
@@ -212,6 +333,37 @@ standalone_pipe_socketpair_one(const size_t * perfsizes, size_t num_perf,
 		warn0("perftest_buffers");
 		goto err0;
 	}
+
+	/* Success! */
+	return (0);
+
+err0:
+	/* Failure! */
+	return (1);
+}
+
+/**
+ * standalone_pipe_truncated_frame():
+ * Verify that decrypt-side EOF is accepted only on an encrypted packet
+ * boundary.
+ */
+int
+standalone_pipe_truncated_frame(void)
+{
+
+	/* Clean EOF before any encrypted data is valid. */
+	if (pipe_decr_eof_case(0, 0, 0))
+		goto err0;
+
+	/* EOF after a complete encrypted frame is valid. */
+	if (pipe_decr_eof_case(1, 0, 0))
+		goto err0;
+
+	/* Any partial encrypted frame at EOF is a connection error. */
+	if (pipe_decr_eof_case(0, 1, -1))
+		goto err0;
+	if (pipe_decr_eof_case(1, PCRYPT_ESZ - 1, -1))
+		goto err0;
 
 	/* Success! */
 	return (0);
